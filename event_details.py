12cd9daf18728e7982c8739c569ad0702b81b8a1
jenniferzxm <jennifr.zhang@mail.utoronto.ca>
2023-10-14 11:53:03 -0400

    most of the static pages, need some formatting fix

--- event_details.py
+++ event_details.py
@@ -8,3 +8,40 @@ bootstrap = Bootstrap(app)
 @app.route('/eventdetails')
 def eventdetails():
     return render_template('event_details.html')
+
+@app.route('/search')
+def search():
+    return render_template('search_tab.html')
+
+@app.route('/bookmark')
+def bookmark():
+    return render_template('bookmark.html')
+
+@app.route('/event_post')
+def event_post():
+    return render_template('event_post.html')
+
+#login 
+#main_dashboard
+
+@app.route('/my_account/event_history')
+def my_account_event_history():
+    return render_template('my_account_eventhistory.html')
+
+@app.route('/my_account/friends')
+def my_account_friends():
+    return render_template('my_account_friends.html')
+
+@app.route('/my_account/myevents')
+def my_account_myevents():
+    return render_template('my_account_myevents.html')
+
+@app.route('/my_account/notification')
+def my_account_notification():
+    return render_template('my_account_notification.html')
+
+# register account
+# @app.route('/register')
+# def register():
+#     return render_template('register.html')
+

413ad169f317ec04b42b0da8dbd35e72f9de1818
Hui Jeong Lee <huijeong1015@gmail.com>
2023-10-25 16:11:06 -0400

    Test that user info exists in my account page

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,36 +1,46 @@
-import unittest
-from .. import "app.py"
+import pytest
+from pathlib import Path
+from project.main import app, db, Event
 
-class BaseTestCase(unittest.TestCase):
+TEST_DB = "test.db"
 
-    def setUp(self):
-        # Ensure we're working with a test database and not the main database
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-        app.config['TESTING'] = True
-        self.app = app.test_client()
-        
-        # Push an application context so we can connect to the database
-        with app.app_context():
-            db.create_all()
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    # Ensure we're working with a test database and not the main database
+    app.config['TESTING'] = True
+    app.config['DATABASE'] = BASE_DIR.joinpath(TEST_DB)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
+    
+    # Push an application context so we can connect to the database
+    with app.app_context():
+        db.create_all() # create db
+        yield app.test_client() # run tests
+        db.drop_all() # remove db
 
-    def tearDown(self):
-        with app.app_context():
-            db.drop_all()
+def test_show_events(client):
+    # Populate the database with a sample event
+    event = Event(name="Sample Event", date="2023-10-15", time="10:00", location="Test Location", description="This is a test event.")
+    with app.app_context():
+        db.session.add(event)
+        db.session.commit()
 
-class FlaskAppTests(BaseTestCase):
+    # Fetch the show_events route
+    response = client.get('/')
+    
+    # Ensure the sample event is present in the response
+    assert "Sample Event", response.data.decode()
 
-    def test_show_events(self):
-        # Populate the database with a sample event
-        event = Event(name="Sample Event", date="2023-10-15", time="10:00", location="Test Location", description="This is a test event.")
-        with app.app_context():
-            db.session.add(event)
-            db.session.commit()
+def login(client, username, password):
+    """Login helper function"""
+    return client.post(
+        "/login",
+        data=dict(username=username, password=password),
+        follow_redirects=True,
+    )
 
-        # Fetch the show_events route
-        response = self.app.get('/')
-        
-        # Ensure the sample event is present in the response
-        self.assertIn("Sample Event", response.data.decode())
-
-if __name__ == "__main__":
-    unittest.main()
+def test_my_account_page_contains_userinfo(client):
+    login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    rv = client.get("/my_account/myevents")
+    assert "HELLO, {:s}!".format(app.config['USERNAME']).encode() in rv.data
+    assert "Interests: {:s}".format(app.config['INTERESTS']).encode() in rv.data

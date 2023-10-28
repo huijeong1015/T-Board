a88b6d5d9b5b3dfd7c9fb2f2cd56917a23adce87
Dasha Mosk <dasha.mosk15@gmail.com>
2023-10-28 16:52:52 -0400

    ALtered db.py to create two databases. Need to fix app.py next

--- project/db.py
+++ project/db.py
@@ -1,29 +1,25 @@
-from flask import Flask, render_template_string
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text  # Correct import for 'text'
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
 from pathlib import Path
 
-# configuration
-DATABASE="events.db"
-USERNAME = "test"
-PASSWORD = "ece444test"
-EMAIL = "test@mail.utoronto.ca"
-INTERESTS = "Computer Engineering"
-
+# Configuration
+USERS_DATABASE = "users.db"
+EVENTS_DATABASE = "events.db"
 basedir = Path(__file__).resolve().parent
 
+# Setting up Flask app instance
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{Path(basedir).joinpath(DATABASE)}"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{Path(basedir).joinpath(EVENTS_DATABASE)}"
+app.config['SQLALCHEMY_BINDS'] = {
+    'users': f"sqlite:///{Path(basedir).joinpath(USERS_DATABASE)}",
+    'events': f"sqlite:///{Path(basedir).joinpath(EVENTS_DATABASE)}"
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Model for events
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +32,21 @@ class Event(db.Model):
     def __repr__(self):
         return f"<Event {self.name}>"
 
+# Model for users
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    interests = db.Column(db.String(255), nullable=True)  
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+sample_users = [
+    {"username": "test", "password": "ece444test", "email": "test@mail.utoronto.ca", "interests": "Computer Engineering"}
+]
+
 sample_events = [
     {"name": "Tech Conference 2023", "date": "2023-11-20", "time": "09:00", "location": "Silicon Valley Convention Center", "description": "Join industry leaders..."},
     {"name": "Music Festival", "date": "2023-08-15", "time": "12:00", "location": "Central Park, New York", "description": "A celebration of music..."},
@@ -43,27 +54,21 @@ sample_events = [
     {"name": "Science Fair", "date": "2023-07-10", "time": "10:00", "location": "Science Museum, London", "description": "Engage with scientific discoveries..."}
 ]
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-
 with app.app_context():
+    # Create tables
     db.create_all()
-    
-    if User.query.first() is None:
-        user = User(username='admin', password='adminpass', email='admin@mail.utoronto.ca')  # Setting password directly
-        db.session.add(user)
-    
-    if Event.query.first() is None:
+
+    # Populate events table
+    if not Event.query.first():
         for event_data in sample_events:
             event = Event(**event_data)
             db.session.add(event)
+        db.session.commit()  
 
-    db.session.commit()
+    # Populate users table
+    if not User.query.first():
+        for user_data in sample_users:
+            user = User(**user_data)
+            db.session.add(user)
+        db.session.commit() 
+

e058f0e114206c6d8535153b33a06de770c872c0
Weihang Zheng <weihang.zheng@mail.utoronto.ca>
2023-10-31 18:56:31 -0400

    all changes from friends but reformatted

--- project/app.py
+++ project/app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    flash,
+    render_template_string,
+)
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -8,6 +17,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from sqlalchemy.sql import func
 from project.db import *
+from werkzeug.security import check_password_hash
+import re
+
 
 app.config["SECRET_KEY"] = os.urandom(24)
 
@@ -28,7 +40,7 @@ def login():
             user = User.query.filter_by(username=username).first()
             if user is None:
                 error = f"No user found with username: {username}"
-            elif user.password != password:  # Directly comparing the plaintext password
+            elif not check_password_hash(user.password, password):
                 error = "Password does not match for the provided username."
             else:
                 # Start a user session
@@ -41,7 +53,6 @@ def login():
 @app.route("/register/", methods=["GET", "POST"])
 def register():
     error = None
-    print("We are registering")
     if request.method == "POST":
         username = request.form["input-id"]
         email = request.form["input-email"]
@@ -50,8 +61,12 @@ def register():
         confirm_password = request.form["input-confirm-pwd"]
         interests = request.form["input-interests"]
 
+        # Check if username or email is already taken
         username_check = User.query.filter_by(username=username).first()
         email_check = User.query.filter_by(email=email).first()
+
+        # Password strength check
+        password_strength = check_password_strength(password)
 
         # Perform validation checks on the form data
         if (
@@ -69,21 +84,38 @@ def register():
         elif password != confirm_password:
             error = "Passwords do not match."
             flash(error)
+        elif password_strength != "Strong":
+            error = f"Password strength is {password_strength}. Please use a stronger password."
+            flash(error)
         elif username_check is not None:
             error = "This Username is taken, please try a different one."
             flash(error)
         elif email_check is not None:
-            error = "This email has already been used. Please return to the login page or use a different email"
+            error = "This email has already been used. Please return to the login page or use a different email."
             flash(error)
         else:
-            # TODO: Need to send email verification
-            new_user = User(
-                username=username, password=password, email=email, interests=interests
-            )
+            new_user = User(username=username, email=email, interests=interests)
+            new_user.set_password(password)  # Hash the password
             db.session.add(new_user)
             db.session.commit()
             return redirect(url_for("login"))
+
     return render_template("register.html")
+
+
+def check_password_strength(password):
+    length = len(password)
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
+
+    if length >= 8 and has_upper and has_lower and has_digit and has_special:
+        return "Strong"
+    elif length >= 6 and has_upper and has_lower and has_digit:
+        return "Medium"
+    else:
+        return "Weak"
 
 
 @app.route("/bookmark/")
@@ -98,7 +130,6 @@ def event_post():
 
 @app.route("/main_dashboard/", methods=["GET", "POST"])
 def main_dashboard():
-    error_msg = ""
     sql = text("SELECT * FROM events;")
     result = db.session.execute(sql)
     if request.method == "POST":
@@ -106,20 +137,17 @@ def main_dashboard():
             event_id = int(request.form["event-details"])
             event = Event.query.filter_by(id=event_id).first()
             return render_template("event_details.html", event=event.__dict__)
-    return render_template("main_dashboard.html", events=result, error_msg=error_msg)
+    return render_template("main_dashboard.html", events=result)
 
 
 @app.route("/search_dashboard/", methods=["POST"])
 def searchEvent():
     keyword = request.form["input-search"]
-    error_msg = ""
     # some error handling before results are used
     results = []
     if keyword:
         results = Event.query.filter(Event.name.contains(keyword)).all()
-    if len(results) == 0:
-        error_msg = "We couldn't find any matches for \"" + keyword + '".'
-    return render_template("main_dashboard.html", events=results, error_msg=error_msg)
+    return render_template("main_dashboard.html", events=results)
 
 
 def get_user_interests():
@@ -207,12 +235,6 @@ def show_events():
     return str(events)
 
 
-@app.route("/users")
-def show_users():
-    users = User.query.all()
-    return render_template("users.html", users=users)
-
-
 @app.route("/event_post", methods=["POST"])
 def add_event():
     event_name = request.form["input-name"]
@@ -240,3 +262,80 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+# DFS Function for Friend Recommendations
+def dfs(graph, start, k):
+    visited, stack = set(), [(start, 0)]
+    recommendations = set()
+    while stack:
+        vertex, depth = stack.pop()
+        if depth <= k and vertex not in visited:
+            visited.add(vertex)
+            if (
+                depth > 0 and vertex not in graph[start]
+            ):  # Exclude start user and their direct friends
+                recommendations.add(vertex)
+            stack.extend((friend, depth + 1) for friend in graph[vertex] - visited)
+    return recommendations
+
+
+@app.route("/users")
+def show_users():
+    users = User.query.all()
+    user_data = {
+        user.username: {
+            "friends": [friend.username for friend in user.friends],
+            "events": [event.name for event in user.events],
+        }
+        for user in users
+    }
+    user_list_html = "<ul>"
+    for username, data in user_data.items():
+        friends = ", ".join(data["friends"]) if data["friends"] else "None"
+        events = ", ".join(data["events"]) if data["events"] else "None"
+        user_list_html += f"<li>{username}: Friends - {friends}, Events - {events}</li>"
+    user_list_html += "</ul>"
+    return render_template_string(
+        f"<h1>Users, Their Friends, and Events</h1>{user_list_html}"
+    )
+
+
+@app.route("/recommendations")
+def friend_recommendations():
+    k = int(request.args.get("k", 2))
+    users = User.query.all()
+    user_data = {
+        user.username: set(friend.username for friend in user.friends) for user in users
+    }
+
+    recommendations_html = "<ul>"
+    for username in user_data:
+        recommendations = dfs(user_data, username, k)
+        recommendations = (
+            recommendations - user_data[username]
+        )  # Filter out existing friends
+        recommendations_html += (
+            f'<li>{username}: Recommendations - {", ".join(recommendations)}</li>'
+        )
+    recommendations_html += "</ul>"
+
+    return render_template_string(
+        f"<h1>Friend Recommendations</h1>{recommendations_html}"
+    )
+
+
+@app.route("/events")
+def new_events():
+    events = Event.query.all()
+    event_data = {
+        event.name: [attendee.username for attendee in event.attendees]
+        for event in events
+    }
+    event_list_html = "<ul>"
+    for event_name, attendees in event_data.items():
+        event_list_html += f'<li>{event_name}: Attendees - {", ".join(attendees) if attendees else "None"}</li>'
+    event_list_html += "</ul>"
+    return render_template_string(
+        f"<h1>Events and Their Attendees</h1>{event_list_html}"
+    )

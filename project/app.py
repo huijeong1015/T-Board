cf0d904b5849d8b1544aae5455752d1ad2d391de
Weihang Zheng <weihang.zheng@mail.utoronto.ca>
2023-10-31 16:13:54 -0400

    add table joiner between users and events

--- project/app.py
+++ project/app.py
@@ -172,8 +172,7 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-### friends
-
+# DFS Function for Friend Recommendations
 def dfs(graph, start, k):
     visited, stack = set(), [(start, 0)]
     recommendations = set()
@@ -181,7 +180,7 @@ def dfs(graph, start, k):
         vertex, depth = stack.pop()
         if depth <= k and vertex not in visited:
             visited.add(vertex)
-            if depth > 0:  # Exclude the start user
+            if depth > 0 and vertex not in graph[start]:  # Exclude start user and their direct friends
                 recommendations.add(vertex)
             stack.extend((friend, depth + 1) for friend in graph[vertex] - visited)
     return recommendations
@@ -189,23 +188,41 @@ def dfs(graph, start, k):
 @app.route('/users')
 def show_users():
     users = User.query.all()
-    user_data = {user.username: user.friends.split(',') if user.friends else [] for user in users}
+    user_data = {
+        user.username: {
+            'friends': [friend.username for friend in user.friends],
+            'events': [event.name for event in user.events]
+        } for user in users
+    }
     user_list_html = '<ul>'
-    for username, friends in user_data.items():
-        user_list_html += f'<li>{username}: Friends - {", ".join(friends) if friends else "None"}</li>'
+    for username, data in user_data.items():
+        friends = ", ".join(data['friends']) if data['friends'] else "None"
+        events = ", ".join(data['events']) if data['events'] else "None"
+        user_list_html += f'<li>{username}: Friends - {friends}, Events - {events}</li>'
     user_list_html += '</ul>'
-    return render_template_string(f"<h1>Users and Their Friends</h1>{user_list_html}")
+    return render_template_string(f"<h1>Users, Their Friends, and Events</h1>{user_list_html}")
 
 @app.route('/recommendations')
 def friend_recommendations():
     k = int(request.args.get('k', 2))
     users = User.query.all()
-    user_data = {user.username: set(user.friends.split(',')) if user.friends else set() for user in users}
+    user_data = {user.username: set(friend.username for friend in user.friends) for user in users}
     
     recommendations_html = '<ul>'
     for username in user_data:
         recommendations = dfs(user_data, username, k)
+        recommendations = recommendations - user_data[username]  # Filter out existing friends
         recommendations_html += f'<li>{username}: Recommendations - {", ".join(recommendations)}</li>'
     recommendations_html += '</ul>'
     
     return render_template_string(f"<h1>Friend Recommendations</h1>{recommendations_html}")
+
+@app.route('/events')
+def new_events():
+    events = Event.query.all()
+    event_data = {event.name: [attendee.username for attendee in event.attendees] for event in events}
+    event_list_html = '<ul>'
+    for event_name, attendees in event_data.items():
+        event_list_html += f'<li>{event_name}: Attendees - {", ".join(attendees) if attendees else "None"}</li>'
+    event_list_html += '</ul>'
+    return render_template_string(f"<h1>Events and Their Attendees</h1>{event_list_html}")
--- project/db.py
+++ project/db.py
@@ -3,19 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
 
 # Configuration
-app = Flask(__name__)
+USERS_DATABASE = "users.db"
+EVENTS_DATABASE = "events.db"
 basedir = Path(__file__).resolve().parent
-USERS_DATABASE = f"sqlite:///{Path(basedir).joinpath('users.db')}"
-EVENTS_DATABASE = f"sqlite:///{Path(basedir).joinpath('events.db')}"
 
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=EVENTS_DATABASE,
-    SQLALCHEMY_BINDS={'users': USERS_DATABASE, 'events': EVENTS_DATABASE},
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-)
+# Setting up Flask app instance
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{Path(basedir).joinpath(EVENTS_DATABASE)}"
+app.config['SQLALCHEMY_BINDS'] = {
+    'users': f"sqlite:///{Path(basedir).joinpath(USERS_DATABASE)}",
+    'events': f"sqlite:///{Path(basedir).joinpath(EVENTS_DATABASE)}"
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-# Models
+# Association table for many-to-many relationship between users and events
+attendees = db.Table('attendees',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('events.id'), primary_key=True)
+)
+
+# Association table for self-referential many-to-many relationship (friends)
+user_friends = db.Table('user_friends',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('friend_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+)
+
+# Model for events
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
@@ -24,10 +40,12 @@ class Event(db.Model):
     time = db.Column(db.String(5), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
+    attendees = db.relationship('User', secondary=attendees, backref=db.backref('events', lazy='dynamic'))
 
     def __repr__(self):
         return f"<Event {self.name}>"
 
+# Model for users
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,18 +53,12 @@ class User(db.Model):
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     interests = db.Column(db.String(255), nullable=True)
-    friends = db.Column(db.String(255), default="")
+    friends = db.relationship('User', secondary=user_friends, primaryjoin=(user_friends.c.user_id == id), secondaryjoin=(user_friends.c.friend_id == id), backref=db.backref('friends_ref', lazy='dynamic'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-# Sample data
-sample_users = [
-    {"username": "admin", "password": "adminpass", "email": "admin@mail.utoronto.ca", "interests": "Being an administrator"},
-    {"username": "a", "password": "password_a", "email": "a@mail.com", "interests": "Interests A", "friends": "b"},
-    {"username": "b", "password": "password_b", "email": "b@mail.com", "interests": "Interests B", "friends": "a,c"},
-    {"username": "c", "password": "password_c", "email": "c@mail.com", "interests": "Interests C", "friends": "b"}
-]
+
 
 sample_events = [
     {"name": "Tech Conference 2023", "date": "2023-11-20", "time": "09:00", "location": "Silicon Valley Convention Center", "description": "Join industry leaders..."},
@@ -55,11 +67,41 @@ sample_events = [
     {"name": "Science Fair", "date": "2023-07-10", "time": "10:00", "location": "Science Museum, London", "description": "Engage with scientific discoveries..."}
 ]
 
-# Database initialization
 with app.app_context():
+    # Create tables
     db.create_all()
+
+    # Populate events table
     if not Event.query.first():
-        db.session.bulk_insert_mappings(Event, sample_events)
+        for event_data in sample_events:
+            event = Event(**event_data)
+            db.session.add(event)
+        db.session.commit()
+
+    # Populate users table, set up friendships, and add users to events
     if not User.query.first():
-        db.session.bulk_insert_mappings(User, sample_users)
-    db.session.commit()
+        admin = User(username="admin", password="adminpass", email="admin@mail.utoronto.ca", interests="Being an administrator")
+        user_a = User(username="user_a", password="password_a", email="a@mail.com", interests="Interests A")
+        user_b = User(username="user_b", password="password_b", email="b@mail.com", interests="Interests B")
+        user_c = User(username="user_c", password="password_c", email="c@mail.com", interests="Interests C")
+
+        # Setting up friendships
+        user_a.friends.append(user_b)  # a and b are friends
+        user_b.friends.append(user_a)  # a and b are friends
+        user_b.friends.append(user_c)  # b and c are friends
+
+        # Adding users to events
+        tech_conference = Event.query.filter_by(name="Tech Conference 2023").first()
+        music_festival = Event.query.filter_by(name="Music Festival").first()
+        charity_run = Event.query.filter_by(name="Charity Run").first()
+
+        user_a.events.append(tech_conference)
+        user_b.events.append(music_festival)
+        user_c.events.append(charity_run)
+
+        db.session.add(admin)
+        db.session.add(user_a)
+        db.session.add(user_b)
+        db.session.add(user_c)
+
+        db.session.commit()

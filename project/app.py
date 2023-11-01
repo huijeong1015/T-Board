b9b77c53cdbb4ee1b6363f38b6e901d7c282dd86
Dasha Mosk <dasha.mosk15@gmail.com>
2023-11-01 14:47:11 -0400

    After creating an event, you are redirected to maindash board

--- project/app.py
+++ project/app.py
@@ -230,7 +230,8 @@ def add_event():
     new_event = Event(name=event_name, date=event_date, time=event_time, location=event_location, description=event_description, event_type=event_type)
     db.session.add(new_event)
     db.session.commit()
-    return render_template('event_post.html', profile_picture=get_user_profile_picture(), event_types=event_type)
+    render_template('event_post.html', profile_picture=get_user_profile_picture(), event_types=event_type)
+    return redirect(url_for("main_dashboard"))
 
 @app.errorhandler(404)
 def page_not_found(e):
